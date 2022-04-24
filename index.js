f1e7d688fe378c4197d3dbaa72b2f14e735bfdf7
mosheurpau <mosheur.pau@gamil.com>
2022-04-25 01:12:03 +0600

    Connect to database with secure password on environment variable

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://dbuser1:<password>@cluster0.zrcp4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrcp4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
